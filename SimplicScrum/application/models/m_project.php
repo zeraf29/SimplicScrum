b7c821160cb90b8e9d89123819994606dbd094cc
김 진협 <zeraf29@gmail.com>
2013-05-13 21:54:05 +0900

    test

--- SimplicScrum/application/models/m_project.php
+++ SimplicScrum/application/models/m_project.php
@@ -6,7 +6,7 @@ class M_project extends SS_Model{
 		$this->db->join('project as p', 'rt.pid = p.id', 'left');
 		$this->db->join('role as r', 'rt.rid = r.id', 'left');
 		$this->db->where('rt.uid', $this->input->cookie('userid'));
-		$this->db->order_by('order', 'ASC');
+		$this->db->order_by('reg_date', 'ASC');
 		$rs = $this->db->get();
 		return ($rs->num_rows() > 0) ? $rs->result() : array();
 	}
