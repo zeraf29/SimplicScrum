18fbb0c2df5aeaf4e50439d8b5f116ba047bd785
김 진협 <zeraf29@gmail.com>
2013-05-13 23:16:02 +0900

    no message

---
+++ SimplicScrum/application/helpers/basic_helper.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// 경고메세지를 경고창으로
+function alert($msg='', $url='') {
+	$CI =& get_instance();
+
+	if (!$msg) $msg = '올바른 방법으로 이용하세요.';
+
+	echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=".$CI->config->item('charset')."\">";
+	echo "<script type='text/javascript'>alert('".$msg."');";
+	if (!$url)
+        echo "history.go(-1);";
+    echo "</script>";
+    if ($url)
+        goto_url($url);
+	exit;
+}
+
+
+// 해당 url로 이동
+function goto_url($url) {
+	echo "<script type='text/javascript'> location.replace('".$url."'); </script>";
+	exit;
+}
+
+?>
