97241777d174bc2e9d82b783c0bf0f16b4b4a338
김 진협 <zeraf29@gmail.com>
2013-05-14 03:15:36 +0900

    no message

--- SimplicScrum/application/hooks/Shooks.php
+++ SimplicScrum/application/hooks/Shooks.php
@@ -3,9 +3,7 @@ class Shooks {
     function checkPermission() {
         $CI =& get_instance();
 
-		$CI->load->helper('cookie');
 		$CI->load->library('session');
-		echo $CI->input->cookie('userid');
 		if ( isset($CI->allow)) {
             if(in_array($CI->router->method, $CI->allow) === false){
 				if ( !$CI->session->userdata("ss_userid") ) { 
