4e524fe5760017b05ffe56d6d82299b2870e1e02
김 진협 <zeraf29@gmail.com>
2013-05-13 23:40:31 +0900

    asdf

--- SimplicScrum/application/controllers/main.php
+++ SimplicScrum/application/controllers/main.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends SS_Controller {
+class Main extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
