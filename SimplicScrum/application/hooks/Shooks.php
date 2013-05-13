b1b3f6471e61496a6186028b181d027ba15bc16e
김 진협 <zeraf29@gmail.com>
2013-05-13 23:02:43 +0900

    no message

--- SimplicScrum/application/hooks/Shooks.php
+++ SimplicScrum/application/hooks/Shooks.php
@@ -4,14 +4,14 @@ class Shooks {
         $CI =& get_instance();        
 		if ( isset($CI->allow)) {
             if(in_array($CI->router->method, $CI->allow) === false){
-				if ( !$CI->input->cookie('mobile') && !$CI->user->isLogin() ) { 
+				if ( !$CI->input->cookie('mobile') && !$CI->session->userdata("userid") ) { 
 					alert('You need to login!','/');
 				}   
 			}else{
 
 			}
         } else{
-			 if ( !$CI->input->cookie('mobile') && !$CI->user->isLogin() ) { 
+			 if ( !$CI->input->cookie('mobile') && !$CI->session->userdata("userid") ) { 
 				alert('You need to login!','/');
 			 }
 		}
