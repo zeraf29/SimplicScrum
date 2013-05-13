67d052035bda10cd7d659d86e522aece17e28578
김 진협 <zeraf29@gmail.com>
2013-05-13 20:21:56 +0900

    no message

--- SimplicScrum/application/models/m_user.php
+++ SimplicScrum/application/models/m_user.php
@@ -1,12 +1,11 @@
 <?php
 class M_user extends SS_Model{
 	function get_login($email,$pw){
-
 		$this->db->select('*');
 		$this->db->from("user");
 		$this->db->where("email",$email);
 		$this->db->where("pw",sha1("$pw"));
-		$data  = $this->db->get()
+		$data  = $this->db->get();
 		return ($rs->num_rows() > 0) ? $rs->result() : array();
 	}
 }
