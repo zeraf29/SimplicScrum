8c7ec4c802778ae6a4cc4699833672106fc6f2c2
김 진협 <zeraf29@gmail.com>
2013-05-20 17:09:52 +0900

    no message

--- gitsync.php
+++ gitsync.php
@@ -1,5 +1,12 @@
 <?php
-
-$last_line = system("git push sscrum master",$retval);
+$last_line = system("su -",$retval);
+echo $last_ine."</br>".$retval;
+$last_line = system("kezn0079",$retval);
+echo $last_ine."</br>".$retval;
+$last_line = system("cd /home/sscrum/public_html",$retval);
+echo $last_ine."</br>".$retval;
+$last_line = system("git init",$retval);
+echo $last_ine."</br>".$retval;
+$last_line = system("git pull sscrum master",$retval);
 echo $last_ine."</br>".$retval;
 ?>
