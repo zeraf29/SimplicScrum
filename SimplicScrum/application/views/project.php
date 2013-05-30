b5260b7881087097a2516d6c7d036c56b0aee323
NaraeHYEON <nil270000000@gmail.com>
2013-05-31 05:38:14 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -94,7 +94,7 @@
 						$str = "";
 						$cnt = 1;
 						foreach($backlog["porduct"] as $key){
-							$str .= "<div id=\"pList_".$key->pd_id."\" class=\"pbacklog_list nlClass\"><div>TITLE</div><div class='title'>".$key->pd_title."</div>";
+							$str .= "<div id=\"pList_".$key->pd_id."\" class=\"pbacklog_list nlClass\"><div style='width:100px; display:inline; margin:10px; font-weight: bold'>TITLE</div><div class='title' style='width:100px; display:inline; border:1px black solid; padding:5px; background:white'>".$key->pd_title."</div>";
 							$str .= "<div class='bContent'><div style = 'font-weight:bold; font-size:10 margin-top:15px'>".$key->pd_desc."</div><div><a href = '#'>modify</a></div></div></div>";
 							$cnt++;
 						}
