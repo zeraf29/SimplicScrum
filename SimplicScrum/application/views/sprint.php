30d6d1f4ba0eed8fe6f431ee5839825e1549b857
NaraeHYEON <nil270000000@gmail.com>
2013-05-31 03:18:47 +0900

    no message

--- SimplicScrum/application/views/sprint.php
+++ SimplicScrum/application/views/sprint.php
@@ -80,6 +80,7 @@
 					<li class = "nlClass"></li>
 					<li class = "nlClass"></li>
 					<li class = "nlClass"></li>
+					<li class = "nlClass"></li>
 					<li class = "bottom" style = "visibility : hidden"></li>
 				</ul>
 			</div>
