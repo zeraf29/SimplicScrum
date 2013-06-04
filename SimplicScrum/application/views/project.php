e44bd995d350d646df67d0046957e71c470d8fd6
김 진협 <zeraf29@gmail.com>
2013-06-04 22:58:20 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -361,7 +361,7 @@
 					        	}
 						    });
 						    if(result==100){
-						    	alert($("div").find("addMlists").html());
+						    	alert($("div").find("addMlists").val());
 						    	if($("div").hasClass("addMlists").val()==nickname){
 						    		$("#amEmail").attr("placeholder", "이미 등록되어 있는 멤버입니다.");
 						    	}else{
