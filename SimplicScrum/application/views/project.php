5a51527053657595553cff01f374b49b05987d7a
LeeSangJun <shaamy@naver.com>
2013-06-10 17:15:25 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -550,7 +550,7 @@
 			{
 				if($(this).hasClass("extText")){
 					$(this).switchClass("extText","nlClass");
-					$(this).html("<div style = 'font-weight:bold; font-size:10'></div>")
+					$(this).html("<div style = 'font-weight:bold; font-size:10'><div class = "sprintBacklog_card_name">TestData1</div><div class = "sprintBacklog_vote_now">12</div><div class = "sprintBacklong_vote_total">25</div></div>")
 				}else{
 					$(this).switchClass("nlClass","extText");
 					$(this).html("<div style = 'font-weight:bold; font-size:10'>Backlog NAME1</div><div style = 'font-weight:bold; font-size:10 margin-top:15px'>discription</div><div><a href = '#'>vote</a> <a href = '#'>modify</div>")
