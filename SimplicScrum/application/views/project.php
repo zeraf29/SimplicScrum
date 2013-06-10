7eebe0207118405dc6349a0f032387f4e41c07fa
김 진협 <zeraf29@gmail.com>
2013-06-11 03:17:51 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -683,10 +683,10 @@
 
 			$("#id_productBacklog_list").children(".pbacklog_list").click(function(){
 				if($(this).hasClass("extText")){
-					$(this).removeClass("extText",1000);
+					$(this).removeClass("extText", {duration:500});
 					$(this).children(".pbacklog_list").children(".bContent").hide();
 				}else{
-					$(this).addClass("extText",1000);
+					$(this).addClass("extText", {duration:500});
 				}
 			});
 			
