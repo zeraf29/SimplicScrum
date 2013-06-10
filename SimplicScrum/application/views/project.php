71df1a44add771d2ff069187f8578bb013946733
LeeSangJun <shaamy@naver.com>
2013-06-11 04:17:19 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -767,11 +767,11 @@
 				$( "#make_splint_window" ).hide();
 				$( "#splint_add_btn" ).click(function() {
 					if($make_sbacklog_flag == 1){
-						$( "#make_splint_window" ).hide( "fold","", 1000);
+						$( "#make_splint_window" ).hide( "blind","", 1000);
 						$make_sbacklog_flag = 0;
 						}
 					else if($make_sbacklog_flag ==0){
-						$( "#make_splint_window" ).show( "fold","", 1000);
+						$( "#make_splint_window" ).show( "blind","", 1000);
 						$make_sbacklog_flag = 1;
 					}
 				});
