0187361ac4785c10b40e642f16d0309ec5262885
LeeSangJun <shaamy@naver.com>
2013-06-10 20:43:16 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -115,34 +115,34 @@
 				<div class = "input_makeBacklog">
 					<div id = "Relation_Backlog_List">
 						<div class = "Relation_Backlog_onList">
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name1</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name2</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name3</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name4</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name5</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
-						<div>
-							<div style = "float:left; width:250px; height:24px">Product_Backlog_name6</div>
-							<div style = "float:Right; width:100px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
-						</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name1</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name2</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name3</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name4</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name5</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
+							<div>
+								<div style = "float:left; width:250px; height:24px">Product_Backlog_name6</div>
+								<div style = "float:Right; width:70px;"><a href="#" id="remove_relation_backlog"><img src="<?=$img_path?>/member_addbtn.png" style="vertical-align: middle;"/></a></div>
+							</div>
 					<!--모든 프로덕트 백로그 리스트가 들어가는 DIVISION-->
 						</div>
 					</div>
