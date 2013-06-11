902b9cac82ebc4dcefa9b13b18839cabb2e38a28
김 진협 <zeraf29@gmail.com>
2013-06-11 10:08:42 +0900

    no message

--- SimplicScrum/application/models/m_backlog.php
+++ SimplicScrum/application/models/m_backlog.php
@@ -18,7 +18,7 @@ class M_backlog extends SS_Model{
 		$this->db->where("sb.pid",$pid);
 		$this->db->order_by('reg_date', 'desc');
 		$rs = $this->db->get();
-		$data = ($rs->num_rows() > 0) ? $rs->result() : array();
+		return $data = ($rs->num_rows() > 0) ? $rs->result() : array();
 	}
 	function get_sprintloglist($pid, $bid=0){
 		$this->db->select();
