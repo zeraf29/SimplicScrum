513d510ff279e68efc3d9c4739e7b61508ae3a67
김 진협 <zeraf29@gmail.com>
2013-06-11 10:19:06 +0900

    no message

--- SimplicScrum/application/views/project.php
+++ SimplicScrum/application/views/project.php
@@ -124,15 +124,14 @@
 				
 				<div class = "input_makeBacklog"><label for = "sbacklog_discription" class ="label_backlog">DISCRIPTION</label><textarea id ="sbacklog_discription" name="sbacklog_discription" style="margin-left:40px; margin-top : 10px; padding : 10px; width:250px; height:50px"></textarea></div>
 				<div class = "input_makeBacklog label_backlog">Relation_Backlog</div>
-				<div class = "input_makeBacklog label_backlog" style = "margin-top:-15px;border-bottom:5px dotted black;width:100px;"></div>
+				<div class = "input_makeBacklog label_backlog" style = "margin-top:-15px;border-bottom:5px dotted #808080;width:250px;"></div>
 					<div id = "relation_backlog_list" style = "margin-top:-15px;" >
 						<div class = "Relation_Backlog_onList">
 						<!--리스트가 들어가는 DIVISION-->
 						</div>
 					</div>
-				<div class = "input_makeBacklog label_backlog"  style = "margin-top:-10px;border-bottom:5px dotted black;width:100px;"></div>
 				<div class = "input_makeBacklog label_backlog"  style = "margin-top:-15px;">Relation_Backlog_List</div>
-				<div class = "input_makeBacklog label_backlog"  style = "margin-top:-15px;border-bottom:5px dotted black;width:100px;"></div>
+				<div class = "input_makeBacklog label_backlog"  style = "margin-top:-15px;border-bottom:5px dotted #808080;width:250px;"></div>
 				<div class = "input_makeBacklog">
 					<div id = "Relation_Backlog_List">
 						<div class = "Relation_Backlog_offList">
